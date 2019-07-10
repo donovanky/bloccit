e68df22e24c11773d43cc65028549431c9093d64
Kyle D <donovanky@my.easternct.edu>
2019-07-10 15:33:47 -0400

    marco

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,21 +1,22 @@
-const request = require("request");
-const server = require("../../src/server");
-const base = "http://localhost:3000/";
+const request = require('request');
+const server = require('../../src/server');
+const base = 'http://localhost:3000';
 
 describe("routes : static", () => {
   describe("GET /", () => {
-    it("should return status code 200 and have 'Welcome to Bloccit'", () => {
+    it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", () => {
       request.get(base, (error, response, body) => {
         expect(response.statusCode).toBe(200);
         expect(body).toContain("Welcome to Bloccit");
       });
     });
   });
+
   describe("GET /marco", () => {
-    it("should return status code 200 and return string polo", done => {
+    it("should return status code 200", done => {
       request.get(base + "/marco", (error, response, body) => {
         expect(response.statusCode).toBe(200);
-        expect(body).toContain('polo');
+        expect(response.body).toBe('polo');
         done();
       });
     });
