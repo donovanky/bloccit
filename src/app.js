971802c77dcc38ce349174d0f939ad7d5238d2c5
Kyle D <donovanky@my.easternct.edu>
2019-07-10 16:40:53 -0400

    production

--- src/app.js
+++ src/app.js
@@ -4,6 +4,6 @@ const app = express();
 const appConfig = require("./config/main-config.js");
 const routeConfig = require("./config/route-config.js");
 
-appConfig.init();
+appConfig.init(app, express);
 routeConfig.init(app);
 module.exports = app;
