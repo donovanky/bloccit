976d67eb0e9ed7845b9d29a76c0d8df710700655
Kyle D <donovanky@my.easternct.edu>
2019-07-15 19:11:24 -0400

    Topic Checkpoint

---
+++ src/controllers/RuleController.js
@@ -0,0 +1,18 @@
+
+ const RuleQueries = require("../db/queries.Rule.js");
+
+ module.exports = {
+   index(request, response, next){
+
+//#2
+      RuleQueries.getAllTopics((error, Rule) => {
+
+//#3
+        if(error){
+          response.redirect(500, "static/index");
+        } else {
+          response.render("Rule/index", {Rule});
+        }
+      })
+   }
+ }
