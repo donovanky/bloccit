24b694e0f48b713554ec45056c351fe4214ff146
Kyle D <donovanky@my.easternct.edu>
2019-08-02 18:16:24 -0400

    errors 2 cont.10

--- src/routes/flairs.js
+++ src/routes/flairs.js
@@ -8,7 +8,7 @@ router.get("/topics/:topicId/posts/:postId/flairs/:id", flairController.show);
 router.get("/topics/:topicId/posts/:postId/flairs/:id/edit", flairController.edit);
 
 router.post("/topics/:topicId/posts/:postId/flairs/create", flairController.create);
-router.post("/topics/:topicId/posts/:postId/flairs/:id/destroy", flairController.destroy);
+router.post("/flairs/:id/destroy", flairController.destroy);
 router.post("/topics/:topicId/posts/:postid/flairs/:id/update", flairController.update);
 
 module.exports = router;
