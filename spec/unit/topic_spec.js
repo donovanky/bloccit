84e3b7d1c498b3c6a9927b8312118d3d8940341e
Kyle D <donovanky@my.easternct.edu>
2019-08-20 19:03:11 -0400

    updated topic_spec

--- spec/unit/topic_spec.js
+++ spec/unit/topic_spec.js
@@ -55,64 +55,46 @@ const User = require("../../src/db/models").User;
 
 describe("Topic", () => {
 
-  beforeEach((done) => {
-//#1
-    this.topic;
-    this.post;
 
-    sequelize.sync({force: true}).then((response) => {
-      Topic.create({
-        title: "Sight seeing",
-        description: "Where did your last road trip take you?"
-      })
-      .then((topic) => {
-        this.topic = topic;
+   beforeEach((done) => {
+     this.topic;
+     this.post;
+     this.user;
 
-       Post.create({
-           title: "Albuquerque",
-           body: "I took a wrong turn",
-           topicId: this.topic.id
+     sequelize.sync({force: true}).then((res) => {
+
+// #2
+       User.create({
+         email: "starman@tesla.com",
+         password: "Trekkie4lyfe"
+       })
+       .then((user) => {
+         this.user = user; //store the user
+
+// #3
+         Topic.create({
+           title: "Expeditions to Alpha Centauri",
+           description: "A compilation of reports from recent visits to the star system.",
+
+// #4
+           posts: [{
+             title: "My first visit to Proxima Centauri b",
+             body: "I saw some rocks.",
+             userId: this.user.id
+           }]
+         }, {
+
+// #5
+           include: {
+             model: Post,
+             as: "posts"
+           }
          })
-         .then((post) => {
-           this.post = post;
+         .then((topic) => {
+           this.topic = topic; //store the topic
+           this.post = topic.posts[0]; //store the post
            done();
-         });
-      })
-        .catch(error => {
-          console.log(error);
-      });
+         })
+       })
+     });
    });
- });
-describe("#create()", () => {
-    it ("should create topic", done => {
-      Topic.create({
-        title: "Sample topic",
-        description: "Just to test"
-      })
-      .then(topic => {
-        expect(topic.title).toBe("Sample topic");
-        expect(topic.description).toBe("Just to test");
-        done();
-      })
-      .catch(error => {
-        console.log(error);
-        done();
-      });
-    });
-  });
-  describe("#getPosts()", () => {
-    it("should retrieve the posts attached to the topic", done => {
-      this.topic.getPosts()
-      .then(associatedPosts => {
-        expect(associatedPosts[0].title).toBe("Albuquerque");
-        expect(associatedPosts[0].body).toBe("I took a wrong turn");
-
-        done();
-      })
-      .catch(error => {
-        console.log(error);
-        done();
-      });
-    });
-  });
-})
