06427e875d4321380f0c0546b5e823a97fee7d69
Kyle D <donovanky@my.easternct.edu>
2019-09-18 17:36:24 -0400

    post spec update

--- spec/integration/posts_spec.js
+++ spec/integration/posts_spec.js
@@ -4,6 +4,35 @@ const Post = require("../../src/db/models").Post;
 const User = require("../../src/db/models").User;
 const Vote = require("../../src/db/models").Vote;
 
+User.create({
+        email: "starman@tesla.com",
+        password: "Trekkie4lyfe"
+      })
+      .then((user) => {
+        this.user = user; //store the user
+
+        Topic.create({
+          title: "Expeditions to Alpha Centauri",
+          description: "A compilation of reports from recent visits to the star system.",
+
+          posts: [{
+            title: "My first visit to Proxima Centauri b",
+            body: "I saw some rocks.",
+            userId: this.user.id
+          }]
+        }, {
+          include: {
+            model: Post,
+            as: "posts"
+          }
+        })
+        .then((topic) => {
+          this.topic = topic; //store the topic
+          this.post = topic.posts[0]; //store the post
+          done();
+        })
+      })
+
 describe("Post", () => {
 
   beforeEach((done) => {
